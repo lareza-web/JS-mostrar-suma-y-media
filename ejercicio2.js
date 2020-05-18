94e523150098d5fac46dcd1651bcc10ebfe5bcc1
lareaza-web <larezaweb@gmail.com>
2020-05-18 20:00:02 -0300

    suma y media

---
+++ ejercicio2.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/*
+Utilizando un bluce, mostrar la suma y la media 
+de los números introducidos por el usuario, hasta 
+introducir un número negativo y ahi mostrar el resultado.
+*/
+
+var suma = 0;
+var contador = 0;
+
+do {
+    var numero = parseInt(prompt('Introduce números hasta que metas uno negativo', 0));
+    if (isNaN(numero)) {
+        numero = 0;
+    } else if (numero >= 0) {
+        suma = suma + numero;
+        //suma += numero;
+        contador++;
+    }
+    console.log(suma);
+    console.log(contador);
+} while (numero >= 0)
+
+alert("La suma de todos los numeros es: " + suma);
+alert("La media de todos los numeros es: " + (suma / contador));
